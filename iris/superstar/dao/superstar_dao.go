07f04a0a7a044acd8cfb2839aef1f2c6c5c1eda0
fans <fan.jinlong@qq.com>
2019-03-11 18:09:49 +0800

    DAO 代码架构搭建

--- iris/superstar/dao/superstar_dao.go
+++ iris/superstar/dao/superstar_dao.go
@@ -1,9 +1,43 @@
 package repositories
 
+// data access object，DAO
 import (
 	"github.com/go-xorm/xorm"
+	"v5u.win/GoLearn/iris/superstar/models"
 )
 
 type SuperstarDao struct {
 	engine *xorm.Engine
+}
+
+func NewSuperstarDao(engine *xorm.Engine) *SuperstarDao {
+
+	return &SuperstarDao{
+		engine: engine,
+	}
+}
+
+func (s *SuperstarDao) Get(id int) *models.StartInfo {
+	return nil
+}
+
+func (s *SuperstarDao) GetAll() []*models.StartInfo {
+	return nil
+}
+
+func (s *SuperstarDao) Search(country string) []*models.StartInfo {
+	return nil
+}
+
+func (s *SuperstarDao) Delete(id int) error {
+	return nil
+}
+
+// columns 判断强制更新
+func (s *SuperstarDao) Update(data *models.StartInfo, columns []string) error {
+	return nil
+}
+
+func (s *SuperstarDao) Create(data *models.StartInfo) error {
+	return nil
 }
--- iris/superstar/datasource/dbhelper.go
+++ iris/superstar/datasource/dbhelper.go
@@ -1,9 +1,12 @@
 package datasource
 
 import (
+	"fmt"
+	"log"
 	"sync"
 
 	"github.com/go-xorm/xorm"
+	"v5u.win/GoLearn/iris/superstar/conf"
 )
 
 var (
@@ -13,11 +16,46 @@ var (
 )
 
 func instanceMaster() *xorm.Engine {
-	// TODO: 单例的实现
+
+	if masterEngine != nil {
+		return masterEngine
+	}
+	// WARNNING:
+	// 互斥锁，如果有多个线程同时访问，且masterEngine == nil 的时候，第一线程创建完后，后续线程依然认为 masterEngine == nil,此时需要在多一次判断
+	lock.Lock()
+	defer lock.Unlock()
+
+	if masterEngine != nil {
+		return masterEngine
+	}
+
+	c := conf.MasterDbConfig
+	connet := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", c.User, c.Pwd, c.Host, c.Port, c.DbName)
+	engine, err := xorm.NewEngine(conf.DriverName, connet)
+	if err != nil {
+		log.Fatal("dbhelper.instanceMaster error=%s", err)
+	}
+	masterEngine = engine
 	return masterEngine
 }
 
 func instanceSlave() *xorm.Engine {
-	// TODO: 单例的实现
+	if slaveEngine != nil {
+		return slaveEngine
+	}
+	lock.Lock()
+	defer lock.Unlock()
+
+	if slaveEngine != nil {
+		return slaveEngine
+	}
+
+	c := conf.SlaveDbConfig
+	connet := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", c.User, c.Pwd, c.Host, c.Port, c.DbName)
+	engine, err := xorm.NewEngine(conf.DriverName, connet)
+	if err != nil {
+		log.Fatal("dbhelper.instanceSlace error=%s", err)
+	}
+	slaveEngine = engine
 	return slaveEngine
 }
