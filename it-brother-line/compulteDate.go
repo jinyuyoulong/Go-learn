cb5b1284c633039905b6f617825c7884dd67e34c
fans <fan.jinlong@qq.com>
2019-08-04 22:41:16 +0800

    闰年判断

---
+++ it-brother-line/compulteDate.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+//闰年的算法：整4不整百，或者整400
+func isLeapYear(theYear int) bool {
+	var isLeapYear bool
+	if theYear%4 == 0 && theYear%100 != 0 {
+		isLeapYear = true
+		return isLeapYear
+	}
+	if theYear%400 == 0 {
+		isLeapYear = true
+		return isLeapYear
+	}
+	return isLeapYear
+}
+func main() {
+	var scanf int
+	for {
+		fmt.Scanln(&scanf)
+		fmt.Println(isLeapYear(scanf))
+	}
+}
