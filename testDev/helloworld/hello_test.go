c2944f165de9028d96c5784fb54feb71738f4125
fans <fan.jinlong@qq.com>
2019-10-17 16:56:58 +0800

    一个新的失败测试，分组测试

--- testDev/helloworld/hello_test.go
+++ testDev/helloworld/hello_test.go
@@ -6,9 +6,19 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got '%q' want '%q' ", got, want)
-	}
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("World")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
 }
