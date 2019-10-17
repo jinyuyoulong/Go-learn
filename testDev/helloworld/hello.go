82710dda0463ebe0ffd1750aa05d4e48e8b94ecf
fans <fan.jinlong@qq.com>
2019-10-17 17:23:29 +0800

    重构3

--- testDev/helloworld/hello.go
+++ testDev/helloworld/hello.go
@@ -12,13 +12,18 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix = englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	prefix := englishHelloPrefix
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
