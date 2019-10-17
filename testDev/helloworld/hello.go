c195a9e37169b6c0a06aad79591aa3a6d7c9588b
fans <fan.jinlong@qq.com>
2019-10-17 17:14:52 +0800

    fix bug

--- testDev/helloworld/hello.go
+++ testDev/helloworld/hello.go
@@ -1,11 +1,17 @@
 // hello
 package main
 
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hella, "
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
--- testDev/helloworld/hello_test.go
+++ testDev/helloworld/hello_test.go
@@ -13,13 +13,13 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("World")
+		got := Hello("World", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
