55b72edabee7b9e7758af32367dff259b66996f0
fans <fan.jinlong@qq.com>
2019-10-17 17:02:03 +0800

    断言重构为函数。减少重复并且提高测试的可读性

--- testDev/helloworld/hello_test.go
+++ testDev/helloworld/hello_test.go
@@ -6,19 +6,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("World")
-		want := "Hello, World"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%q' want '%q'", got, want)
 		}
+	}
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("World")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
