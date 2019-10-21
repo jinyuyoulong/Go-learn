87ef5f607b569d5036bc940dda86b29418d5de6d
fans <fan.jinlong@qq.com>
2019-10-21 18:01:00 +0800

    使用接口重构

--- testDev/struct_func/perematar.go
+++ testDev/struct_func/perematar.go
@@ -1,14 +1,30 @@
 package main
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	width  float64
 	height float64
+}
+
+type Cycle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return rectangle.width*2 + rectangle.height*2
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Cycle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- testDev/struct_func/perematar_test.go
+++ testDev/struct_func/perematar_test.go
@@ -14,10 +14,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{width: 10.0, height: 10.0}
-	got := Area(rectangle)
-	want := 100.0
-	if got != want {
-		t.Errorf("want '%.2f' but got '%.2f'", want, got)
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("want '%.2f' but got '%.2f'", want, got)
+		}
 	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{width: 10.0, height: 10.0}
+
+		want := 100.0
+		checkArea(t, rectangle, want)
+
+	})
+	t.Run("cycle", func(t *testing.T) {
+		cycle := Cycle{10.0}
+		want := 314.1592653589793
+		checkArea(t, cycle, want)
+	})
 }
