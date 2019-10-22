e0dcab9ba95b4a0ddd9e81559fbc8bf21408425b
fans <fan.jinlong@qq.com>
2019-10-22 14:28:41 +0800

    方法改为绑定到指针实例上，对同一个实例进行操作

--- testDev/pointer_error/wallet.go
+++ testDev/pointer_error/wallet.go
@@ -6,10 +6,10 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposite(amount int) {
+func (w *Wallet) Deposite(amount int) {
 	fmt.Println("addr ", &w.balance)
 	w.balance = amount
 }
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
