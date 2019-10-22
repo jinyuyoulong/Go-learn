bfeb70823ecceb5c4ba49fceb6f108b5af58988d
fans <fan.jinlong@qq.com>
2019-10-22 15:25:51 +0800

    重构

--- testDev/pointer_error/wallet_test.go
+++ testDev/pointer_error/wallet_test.go
@@ -5,23 +5,21 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposite", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposite(10)
+	checkBlance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-		want := Bitcoin(20)
 		if got != want {
 			t.Errorf("wallet balance got %s but want %s", got, want)
 		}
+	}
+	t.Run("deposite", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposite(10)
+		checkBlance(t, wallet, Bitcoin(20))
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(10)
-		got := wallet.Balance()
-		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("wallet balance got %s but want %s", got, want)
-		}
+		checkBlance(t, wallet, Bitcoin(10))
 	})
 
 }
