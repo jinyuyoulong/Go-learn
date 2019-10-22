3b0509ae813e1a1a7fffaf0d0b2dd9577ee5097b
fans <fan.jinlong@qq.com>
2019-10-22 14:26:59 +0800

    绑定方法中的实例是值拷贝，wallet 地址不同

--- testDev/pointer_error/wallet.go
+++ testDev/pointer_error/wallet.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposite(amount int) {
+	fmt.Println("addr ", &w.balance)
 	w.balance = amount
 }
 func (w Wallet) Balance() int {
--- testDev/pointer_error/wallet_test.go
+++ testDev/pointer_error/wallet_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposite(10)
+	fmt.Println("addr ", &wallet.balance)
 	got := wallet.Balance()
 	want := 10
 	if got != want {
