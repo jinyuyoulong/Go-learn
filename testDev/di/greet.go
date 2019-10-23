5b10341dc1f6f48fc6a8eca8459e9d64d09ebe22
fans <fan.jinlong@qq.com>
2019-10-23 16:03:07 +0800

    测试重构的通用性

--- testDev/di/greet.go
+++ testDev/di/greet.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-func Greet(b *bytes.Buffer, name string) {
+//Greet 问候
+// 参数 bytes.Buffer 改为 io.Writer 使方法更通用
+func Greet(b io.Writer, name string) {
 	fmt.Fprintf(b, "hello, %s", name)
+}
+
+func myGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "fan")
+}
+func main() {
+	// Greet(os.Stdout, "dd")
+	http.ListenAndServe(":5000", http.HandlerFunc(myGreetHandler))
 }
