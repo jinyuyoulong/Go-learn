8e29c9555db288d852b73f1e8433521e9d9865e3
fans <fan.jinlong@qq.com>
2019-10-23 16:24:28 +0800

    test pass

--- testDev/mocking/mock.go
+++ testDev/mocking/mock.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 
 // 依赖注入 模拟
 
-func CountDown() {
+func CountDown(w io.Writer) {
 	for i := 0; i < 3; i++ {
-		fmt.Println(3 - i)
+		fmt.Fprintf(w, "%d\n", 3-i)
 		time.Sleep(time.Second * 1)
 		if i == 2 {
-			fmt.Println("GO!")
+			fmt.Fprint(w, "GO!")
 		}
 	}
 }
 func main() {
-	CountDown()
+	CountDown(os.Stdout)
 }
--- testDev/mocking/mock_test.go
+++ testDev/mocking/mock_test.go
@@ -1,8 +1,20 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestCountDown(t *testing.T) {
-
-	CountDown()
+	b := &bytes.Buffer{}
+	CountDown(b)
+	got := b.String()
+	// 反引号语法是创建 string 的另一种方式，但是允许你放置东西例如放到新的一行，对我们的测试来说是完美的。
+	want := `3
+2
+1
+GO!`
+	if got != want {
+		t.Errorf("got %s,but want %s", got, want)
+	}
 }
